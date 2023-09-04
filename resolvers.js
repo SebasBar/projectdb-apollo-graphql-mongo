245b28690a054249a2a998e027563eed83008218
Sebas <toolt2y@gmail.com>
2023-09-04 12:51:51 +0200

    Connection to mongodb working

--- resolvers.js
+++ resolvers.js
@@ -3,17 +3,7 @@ const { Projects } = require("./models/Project");
 const resolvers = {
     Query: {
         projects: async () => await Projects.find({}),
-        project: async (_, args) => await Projects.find(args.name),
-        // description: () => 'description',
-        // description2: () => 'description2',
-        // description3: () => 'description3',
-        // github_link: () => 'github_link',
-        // deployed_link: () => 'deployed_link',
-        // pictures: () => 'pictures',
-        // tech_lang: (_, args) => [{ name: args.name, description: args.description }],
-        // clients: (_, args) => [{ name: args.name, description: args.description }],
-        // teammates: (_, args) => [{ name: args.name, description: args.description }]
-
+        project: async (_, args) => await Projects.findOne({ name: args.name }),
     },
 };
 
